f78160b4600f98ff82de4a121f3ce97bdcd0d8ec
Vinicius de Medeiros <vmeedeiros@gmail.com>
2022-02-25 17:47:22 -0300

    Adiciona requisitos 18 e 19

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const positionEmail = document.querySelector('#email-header');
 const positionPassword = document.querySelector('#password-header');
 const positionButton = document.querySelector('#button-header');
+const positionSubmit = document.querySelector('#submit-btn');
+const positionCheckBox = document.querySelector('#agreement');
 
 function verificaLogin() {
   if (positionEmail.value === 'tryber@teste.com' && positionPassword.value === '123456') {
@@ -10,6 +12,15 @@ function verificaLogin() {
   }
 }
 
+function verificaChecked() {
+  if (positionCheckBox.checked === true) {
+    positionSubmit.removeAttribute('disabled');
+  } else {
+    positionSubmit.setAttribute('disabled', '');
+  }
+}
+
 window.onload = function aplicacao() {
   positionButton.addEventListener('click', verificaLogin);
+  positionCheckBox.addEventListener('click', verificaChecked);
 };
