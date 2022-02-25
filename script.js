33f25e405196847695a3fd5f43e7464e514cdca2
Vinicius de Medeiros <vmeedeiros@gmail.com>
2022-02-25 18:52:41 -0300

    Correcao

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const positionPassword = document.querySelector('#password-header');
 const positionButton = document.querySelector('#button-header');
 const positionSubmit = document.querySelector('#submit-btn');
 const positionCheckBox = document.querySelector('#agreement');
+const positionTextarea = document.querySelector('#textarea');
+const valordeP = document.querySelector('#counter');
 
 function verificaLogin() {
   if (positionEmail.value === 'tryber@teste.com' && positionPassword.value === '123456') {
@@ -20,7 +22,20 @@ function verificaChecked() {
   }
 }
 
+function subtrai() {
+  const valor = Number(valordeP.innerText);
+  valordeP.innerText = valor - 1;
+}
+
+// function soma() {
+//   const valordeP = document.querySelector('#counter');
+//   const valor = Number(valordeP.innerText);
+//   valordeP.innerText = valor + 1;
+// }
+
 window.onload = function aplicacao() {
   positionButton.addEventListener('click', verificaLogin);
   positionCheckBox.addEventListener('click', verificaChecked);
+  positionTextarea.addEventListener('keypress', subtrai);
+  // positionTextarea.addEventListener('keydown', soma);
 };
