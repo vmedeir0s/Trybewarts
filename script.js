52e71cbffd8b5a232c36a826b4c72134e79ae785
Vinicius de Medeiros <vmeedeiros@gmail.com>
2022-02-28 17:40:26 -0300

    Adiciona requisito 21

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const positionSubmit = document.querySelector('#submit-btn');
 const positionCheckBox = document.querySelector('#agreement');
 const positionTextarea = document.querySelector('#textarea');
 const valordeP = document.querySelector('#counter');
+const positionForm = document.querySelector('#evaluation-form');
 
 function verificaLogin() {
   if (positionEmail.value === 'tryber@teste.com' && positionPassword.value === '123456') {
@@ -28,9 +29,50 @@ function contagemLimite() {
   valordeP.innerHTML = positionTextarea.maxLength - positionTextarea.value.length;
 }
 
+function checkRadio(value) {
+  const families = document.querySelectorAll(value);
+  for (let i = 0; i < families.length; i += 1) {
+    if (families[i].checked) {
+      return families[i].value;
+    }
+  }
+}
+
+function checkboxValues() {
+  const arrayCheckbox = document.querySelectorAll('.subject');
+  let text = 'Matérias: ';
+  for (let i = 0; i < arrayCheckbox.length; i += 1) {
+    if (arrayCheckbox[i].checked) {
+      text += arrayCheckbox[i].value;
+      text += ', ';
+    }
+  }
+  return text;
+}
+
+function personalInformation() {
+  const name = document.querySelector('#input-name').value;
+  const lastName = document.querySelector('#input-lastname').value;
+  const email = document.querySelector('#input-email').value;
+  const house = document.querySelector('#house').value;
+  const familia = checkRadio('#familia input[type=radio]');
+  const subjects = checkboxValues();
+  const rate = checkRadio('#rate input[type=radio]');
+  const textarea = document.querySelector('#textarea').value;
+  positionForm.innerHTML = '';
+  positionForm.innerHTML += `Nome: ${name} ${lastName}<br>`;
+  positionForm.innerHTML += `Email: ${email}<br>`;
+  positionForm.innerHTML += `Casa: ${house}<br>`;
+  positionForm.innerHTML += `Família: ${familia}<br>`;
+  positionForm.innerHTML += `${subjects}<br>`;
+  positionForm.innerHTML += `Avaliação: ${rate}<br>`;
+  positionForm.innerHTML += `Observações: ${textarea}`;
+}
+
 window.onload = function aplicacao() {
   positionButton.addEventListener('click', verificaLogin);
   positionCheckBox.addEventListener('click', verificaChecked);
   // Nosso erro estava aqui: o evento era o Input
   positionTextarea.addEventListener('input', contagemLimite);
+  positionSubmit.addEventListener('click', personalInformation);
 };
